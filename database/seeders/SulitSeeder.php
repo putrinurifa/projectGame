4a83fbc681eebf499c2aed7b386f1a2743bf2567
fauzanmuh <f4uz4n.oioi@gmail.com>
2021-05-12 15:04:36 +0700

    Pembetulan soal dan jawaban pada seeder

--- database/seeders/SulitSeeder.php
+++ database/seeders/SulitSeeder.php
@@ -17,7 +17,7 @@ class SulitSeeder extends Seeder
         $soal = [
         'Apa nama rumah adat di atas? (Langsung sebutkan nama rumah)',
         'Kalimat yang sesuai untuk penulisan hubungan angka 5 dan 7 pada gambar berikut adalah',
-        'Huruf yang tepat untuk melengkapi kata tersebut adalah',
+        'Paman ingin menarik logam dan memisahkan dari barang rongsokan lainnya menggunakan alat seperti pada di gambar. Gaya yang digunakan pada alat tersebut adalah',
         'Susunan kata yang tepat adalah',
         'Perhatikan gambar!
         Perubahan yang dialami es krim tersebut adalah',
@@ -26,11 +26,11 @@ class SulitSeeder extends Seeder
 
         $image = ['soal-sulit-1.jpg', 'soal-sulit-2.jpg', 'soal-sulit-3.jpg', 'soal-sulit-4.jpg', 'soal-sulit-5.jpg', 'soal-sulit-6.jpg'];
         
-        $jawaban = ['gadang', '7 lebih dari 5', 'h dan b', 'istirahat', 'mencair', 'kulkas'];
+        $jawaban = ['gadang', '7 lebih dari 5', 'magnet', 'istirahat', 'mencair', 'kulkas'];
 
         $kategori = ['Sulit', 'Sulit', 'Sulit', 'Sulit', 'Sulit', 'Sulit'];
 
-        $skor = [35, 32, 36, 35, 40, 38];
+        $skor = [35, 32, 39, 35, 40, 38];
 
         $no = [1, 2, 3, 4, 5, 6];
 
