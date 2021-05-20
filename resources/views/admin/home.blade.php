e8a5e2299127b9a9c96b0eb617b15b64690d66d4
Almasyriqi <ikramalmasyriqi@gmail.com>
2021-05-20 23:18:48 +0700

    setup admin lte

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,28 +1,13 @@
-@extends('layouts.app')
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-
-                    <table class="table table-responsive">
-                        <tr><th>Name</th><th>:</th><td>{{$user->name}}</td></tr>
-                        <tr><th>Email</th><th>:</th><td>{{$user->email}}</td></tr>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+    <h2>Hello {{$user->name}}</h2>
+    <p>Welcome to this beautiful admin panel.</p>
+    
+@stop
